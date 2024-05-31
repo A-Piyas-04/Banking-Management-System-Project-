480b38871f0fa36887892af3c3e66144449774d7
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2024-05-31 22:52:32 +0600

    Added features in Second Page of Create account

--- Bank Management System/src/CreateAccountSecondPage.java
+++ Bank Management System/src/CreateAccountSecondPage.java
@@ -7,166 +7,163 @@ public class CreateAccountSecondPage extends JFrame implements ActionListener {
     JComboBox comboBox,comboBox2,comboBox3,comboBox4,comboBox5;
     JTextField textPan,textAadhar;
     JRadioButton r1,r2, e1,e2;
-    JButton next;
+    JButton next,s,c;
     String formno;
     CreateAccountSecondPage(String formno){
         super("APPLICATION FORM");
 
 
         this.formno = formno;
+        
+        JLabel labelReligion = new JLabel("Religion :");
+        labelReligion.setFont(new Font("Raleway", Font.BOLD,18));
+        labelReligion.setBounds(100,120,100,30);
+        add(labelReligion);
 
-        JLabel l1 = new JLabel("Page 2 :-");
-        l1.setFont(new Font("Raleway", Font.BOLD,22));
-        l1.setBounds(300,30,600,40);
-        add(l1);
-
-        JLabel l2 = new JLabel("Additonal Details");
-        l2.setFont(new Font("Raleway", Font.BOLD,22));
-        l2.setBounds(300,60,600,40);
-        add(l2);
-
-        JLabel l3 = new JLabel("Religion :");
-        l3.setFont(new Font("Raleway", Font.BOLD,18));
-        l3.setBounds(100,120,100,30);
-        add(l3);
-
-        String religion[] = {"Hindu","Muslim","Sikh", "Christian", "Other"};
+        String religion[] = {"Muslim","Hindu","Sikh", "Christian", "Other"};
         comboBox = new JComboBox(religion);
-        comboBox.setBackground(new Color(252,208,76));
+        comboBox.setBackground(new Color(216, 236, 236));
         comboBox.setFont(new Font("Raleway",Font.BOLD,14));
         comboBox.setBounds(350,120,320,30);
         add(comboBox);
 
-        JLabel l4 = new JLabel("Category : ");
-        l4.setFont(new Font("Raleway", Font.BOLD,18));
-        l4.setBounds(100,170,100,30);
-        add(l4);
 
-        String Category [] = {"General","OBC","SC", "ST", "Other"};
-        comboBox2 = new JComboBox(Category);
-        comboBox2.setBackground(new Color(252,208,76));
-        comboBox2.setFont(new Font("Raleway",Font.BOLD,14));
-        comboBox2.setBounds(350,170,320,30);
-        add(comboBox2);
-
-        JLabel l5 = new JLabel("Income : ");
-        l5.setFont(new Font("Raleway", Font.BOLD,18));
-        l5.setBounds(100,220,100,30);
-        add(l5);
+        JLabel labelIncome = new JLabel("Income : ");
+        labelIncome.setFont(new Font("Raleway", Font.BOLD,18));
+        labelIncome.setBounds(100,170,100,30);
+        add(labelIncome);
 
         String income [] = {"Null","<1,50,000","<2,50,000", "5,00,000", "Uptp 10,00,000","Above 10,00,000"};
         comboBox3 = new JComboBox(income);
-        comboBox3.setBackground(new Color(252,208,76));
+        comboBox3.setBackground(new Color(216, 236, 236));
         comboBox3.setFont(new Font("Raleway",Font.BOLD,14));
-        comboBox3.setBounds(350,220,320,30);
+        comboBox3.setBounds(350,170,320,30);
         add(comboBox3);
 
-        JLabel l6 = new JLabel("Educational : ");
-        l6.setFont(new Font("Raleway", Font.BOLD,18));
-        l6.setBounds(100,270,150,30);
-        add(l6);
+        JLabel labelEducation = new JLabel("Educational : ");
+        labelEducation.setFont(new Font("Raleway", Font.BOLD,18));
+        labelEducation.setBounds(100,220,150,30);
+        add(labelEducation);
 
         String educational [] = {"Non-Graduate","Graduate","Post-Graduate", "Doctrate", "Others"};
         comboBox4 = new JComboBox(educational);
-        comboBox4.setBackground(new Color(252,208,76));
+        comboBox4.setBackground(new Color(216, 236, 236));
         comboBox4.setFont(new Font("Raleway",Font.BOLD,14));
-        comboBox4.setBounds(350,270,320,30);
+        comboBox4.setBounds(350,220,320,30);
         add(comboBox4);
 
 
-        JLabel l7 = new JLabel("Occupation : ");
-        l7.setFont(new Font("Raleway", Font.BOLD,18));
-        l7.setBounds(100,340,150,30);
-        add(l7);
+        JLabel labelOccupation = new JLabel("Occupation : ");
+        labelOccupation.setFont(new Font("Raleway", Font.BOLD,18));
+        labelOccupation.setBounds(100,270,150,30);
+        add(labelOccupation);
 
         String Occupation [] = {"Salaried","Self-Employed","Business", "Student", "Retired", "Other"};
         comboBox5 = new JComboBox(Occupation);
-        comboBox5.setBackground(new Color(252,208,76));
+        comboBox5.setBackground(new Color(216, 236, 236));
         comboBox5.setFont(new Font("Raleway",Font.BOLD,14));
-        comboBox5.setBounds(350,340,320,30);
+        comboBox5.setBounds(350,270,320,30);
         add(comboBox5);
 
-        JLabel l8 = new JLabel("PAN Number : ");
-        l8.setFont(new Font("Raleway", Font.BOLD,18));
-        l8.setBounds(100,390,150,30);
-        add(l8);
 
-        textPan = new JTextField();
-        textPan.setFont(new Font("Raleway", Font.BOLD,18));
-        textPan.setBounds(350,390,320,30);
-        add(textPan);
+        JLabel labelform = new JLabel("Form No : ");
+        labelform.setFont(new Font("Raleway", Font.BOLD,14));
+        labelform.setBounds(700,10,100,30);
+        add(labelform);
 
-        JLabel l9 = new JLabel("Aadhar Number : ");
-        l9.setFont(new Font("Raleway", Font.BOLD,18));
-        l9.setBounds(100,440,180,30);
-        add(l9);
-
-        textAadhar = new JTextField();
-        textAadhar.setFont(new Font("Raleway", Font.BOLD,18));
-        textAadhar.setBounds(350,440,320,30);
-        add(textAadhar);
+        JLabel labelformText = new JLabel(formno);
+        labelformText.setFont(new Font("Raleway", Font.BOLD,14));
+        labelformText.setBounds(760,10,60,30);
+        add(labelformText);
 
 
-        JLabel l10 = new JLabel("Senior Citizen : ");
-        l10.setFont(new Font("Raleway", Font.BOLD,18));
-        l10.setBounds(100,490,180,30);
-        add(l10);
+//=======================================================================================
+        this.formno = formno;
 
-        r1 = new JRadioButton("Yes");
-        r1.setFont(new Font("Raleway", Font.BOLD,14));
-        r1.setBackground(new Color(252,208,76));
-        r1.setBounds(350,490,100,30);
+        JLabel labelAccDetail = new JLabel("Account Details");
+        labelAccDetail.setFont(new Font("Raleway",Font.BOLD,22));
+        labelAccDetail.setBounds(280,70,400,40);
+        add(labelAccDetail);
+
+        JLabel labelAccType = new JLabel("Account Type:");
+        labelAccType.setFont(new Font("Raleway",Font.BOLD,18));
+        labelAccType.setBounds(100,340,200,30);
+        add(labelAccType);
+
+        r1 = new JRadioButton("Saving Account");
+        r1.setFont(new Font("Raleway",Font.BOLD,16));
+        r1.setBackground(new Color(215,252,252));
+        r1.setBounds(100,370,150,30);
         add(r1);
-        r2 = new JRadioButton("No");
-        r2.setFont(new Font("Raleway", Font.BOLD,14));
-        r2.setBackground(new Color(252,208,76));
-        r2.setBounds(460,490,100,30);
+
+        r2 = new JRadioButton("Current Account");
+        r2.setFont(new Font("Raleway",Font.BOLD,16));
+        r2.setBackground(new Color(215,252,252));
+        r2.setBounds(350,370,300,30);
         add(r2);
 
-        JLabel l11 = new JLabel("Existing Account : ");
-        l11.setFont(new Font("Raleway", Font.BOLD,18));
-        l11.setBounds(100,540,180,30);
-        add(l11);
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(r1);
+        buttonGroup.add(r2);
 
-        e1 = new JRadioButton("Yes");
-        e1.setFont(new Font("Raleway", Font.BOLD,14));
-        e1.setBackground(new Color(252,208,76));
-        e1.setBounds(350,540,100,30);
-        add(e1);
-        e2 = new JRadioButton("No");
-        e2.setFont(new Font("Raleway", Font.BOLD,14));
-        e2.setBackground(new Color(252,208,76));
-        e2.setBounds(460,540,100,30);
-        add(e2);
 
-        JLabel l12 = new JLabel("Form No : ");
-        l12.setFont(new Font("Raleway", Font.BOLD,14));
-        l12.setBounds(700,10,100,30);
-        add(l12);
+        JLabel labelCardNum = new JLabel("Card Number:");
+        labelCardNum.setFont(new Font("Raleway",Font.BOLD,18));
+        labelCardNum.setBounds(100,440,200,30);
+        add(labelCardNum);
 
-        JLabel l13 = new JLabel(formno);
-        l13.setFont(new Font("Raleway", Font.BOLD,14));
-        l13.setBounds(760,10,60,30);
-        add(l13);
+        JLabel labelCardNumText = new JLabel("XXXX-XXXX-XXXX-"+formno);
+        labelCardNumText.setFont(new Font("Raleway",Font.BOLD,18));
+        labelCardNumText.setBounds(330,440,250,30);
+        add(labelCardNumText);
 
-        next = new JButton("Next");
-        next.setFont(new Font("Raleway",Font.BOLD,14));
-        next.setBackground(Color.WHITE);
-        next.setForeground(Color.BLACK);
-        next.setBounds(570,640,100,30);
-        next.addActionListener(this);
-        add(next);
 
+        JLabel labelPIN = new JLabel("PIN:");
+        labelPIN.setFont(new Font("Raleway",Font.BOLD,18));
+        labelPIN.setBounds(100,490,200,30);
+        add(labelPIN);
+
+        JLabel labelPINtext = new JLabel("XXXX");
+        labelPINtext.setFont(new Font("Raleway",Font.BOLD,18));
+        labelPINtext.setBounds(330,490,200,30);
+        add(labelPINtext);
+
+
+        JCheckBox c7 = new JCheckBox("I here by decleares that the above entered details correct to the best of my knlowledge.",true);
+        c7.setBackground(new Color(215,252,252));
+        c7.setFont(new Font("Raleway",Font.BOLD,12));
+        c7.setBounds(100,640,600,20);
+        add(c7);
+
+
+
+        s = new JButton("Submit");
+        s.setFont(new Font("Raleway", Font.BOLD,14));
+        s.setBackground(Color.BLACK);
+        s.setForeground(Color.WHITE);
+        s.setBounds(215,670,100,30);
+        s.addActionListener(this);
+        add(s);
+
+        c = new JButton("Cancel");
+        c.setFont(new Font("Raleway", Font.BOLD,14));
+        c.setBackground(Color.BLACK);
+        c.setForeground(Color.WHITE);
+        c.setBounds(570,670,100,30);
+        c.addActionListener(this);
+        add(c);
+//=======================================================================================
 
         setLayout(null);
         setSize(850,750);
         setLocation(450,80);
-        getContentPane().setBackground(new Color(252, 208, 76));
+        getContentPane().setBackground(new Color(85, 188, 194));
         setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
         String rel = (String) comboBox.getSelectedItem();
         String cate = (String) comboBox2.getSelectedItem();
         String inc = (String) comboBox3.getSelectedItem();
